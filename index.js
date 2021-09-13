46b9d73cf4c7a421260c7e1d1f8bb83b5707c7ac
Badger42S <57646489+Badger42S@users.noreply.github.com>
2021-09-13 13:38:56 +0300

    changed variable names

--- index.js
+++ index.js
@@ -5,29 +5,31 @@ window.onload = () => {
     const changeButton = document.getElementById('changeButton');
 
     const changeColorRectangle = ( toplength, leftlength) => {
-        mainContainer.innerHTML = '';
-
         const top = toplength > 0 ? toplength: 10;
         const left = leftlength > 0 ? leftlength: 10;
 
+        mainContainer.innerHTML = '';
+        mainContainer.style.height = `${top*50 + 20}px`;
+        mainContainer.style.width = `${left*50 + 20}px`;
+
         for(let i = 0; i < top; i++) {
             for(let j = 0; j < left; j++) {
-                let squareEl = document.createElement('div');
-                squareEl.style.height = '50px';
-                squareEl.style.width = '50px';
-                squareEl.style.border = '1px solid black';
+                let rectangleEl = document.createElement('div');
+                rectangleEl.style.height = '50px';
+                rectangleEl.style.width = '50px';
+                rectangleEl.style.border = '1px solid black';
                 
                 let h,l =0;
                 h = 300*i/top;
                 l = 50 - 3*j;
             
-                squareEl.onmouseenter =  event => {
+                rectangleEl.onmouseenter =  event => {
                     event.target.style.backgroundColor = `hsl(${h}, 100%, ${l}%)`;
                 };
-                squareEl.onmouseleave =  event => {
+                rectangleEl.onmouseleave =  event => {
                     event.target.style.backgroundColor = 'transparent';
                 };
-                mainContainer.appendChild(squareEl);
+                mainContainer.appendChild(rectangleEl);
             };
         };
     };
